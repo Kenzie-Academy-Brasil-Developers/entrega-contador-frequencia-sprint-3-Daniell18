d6883e6e5657d723ee071f4029aab6e9fa621e79
Daniel Francisco da Silva <daniel_mp18@outlook.com>
2021-07-15 16:23:39 -0300

    final-version

---
+++ script.js
@@ -0,0 +1,92 @@
+let button=document.getElementById("button")
+let button2=document.getElementById("button2")
+function captaTexto (){
+    let texto=document.getElementById("textinput").value;
+    return texto;
+}
+button.addEventListener('click',function a(){
+    let result_letra=document.getElementById("teste")
+    main(captaTexto(),1,result_letra)
+})
+button2.addEventListener('click',function b(){
+    let result_palavra=document.getElementById("teste1")
+    main(captaTexto(),2,result_palavra)
+})
+function main(texto,seletor,result){
+    texto=texto.toLowerCase();
+    texto=texto.replace(/[^a-z'\s]+/g, "");
+    let palavras = texto.split(/\s/);
+    texto=texto.replace(/\s/,"");
+    let letras=texto.split("")
+    let escolha=[]
+    let contador2=0;
+    let array=[]
+    let x=[];
+    let contador=0;
+    let frase;
+ 
+    if(seletor===1){
+        escolha=letras
+        frase="A quantidade de letras  foi :"
+    }
+    if(seletor===2){
+        frase="A quantidade de palavras  foi :"
+        escolha=palavras;
+    }
+    separaString(array,escolha,contador2,x)
+   
+    limpaString(x)
+   
+    contaLetras(contador,x,escolha)
+    
+    x=x.join("// ")
+    imprime(x,result,frase)
+}
+function separaString (array,texto,contador2,x){
+    for(let i=0;i<texto.length;i++){
+        array[i]=texto[i]
+        contador2=0
+        for(let j=0;j<texto.length;j++){
+            
+            if(texto[i]===array[j]){
+            contador2++;
+            }
+            if(contador2===1){
+          
+                x[i]=texto[i]    
+            }
+            if(contador2>1){
+              
+                x[i]="#"
+            }
+        }
+    }
+}
+function limpaString(x){
+    for(let u=0;u<x.length;u++){
+        if(x[u]==="#"){
+            console.log(x[u])
+            x.splice(u,1)
+            u=0;
+        }
+     }
+}
+function contaLetras (contador,x,texto){
+    for(let r=0;r<x.length;r++){
+        contador=0;
+        for(let y=0;y<texto.length;y++){
+            if(x[r]===texto[y]){
+              contador++
+            }
+        }
+       x[r]= x[r]+ ":"+contador
+    }
+}
+function imprime (x,result,frase){
+    let paragrafo=document.createElement("p")
+    let o=document.createTextNode(frase)
+    let i=document.createTextNode(x)
+    result.appendChild(paragrafo)
+    paragrafo.appendChild(o)
+   result.appendChild(i)
+}
